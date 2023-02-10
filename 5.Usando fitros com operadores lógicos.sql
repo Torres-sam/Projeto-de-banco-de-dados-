24479b12e281f93f45c08e92932a07fb94c4601f
Torres-sam <samuelvianatorres@gmial.com>
2023-02-09 23:37:49 -0300

    Add Query de filtros e inserindo dados na tabela aluno

---
+++ 5.Usando fitros com operadores lógicos.sql
@@ -0,0 +1,20 @@
+﻿//Querys da aula
+use universidade_u;
+
+select * from aluno;
+
+select idade, telefone, nome from aluno;
+
+select * from aluno where sexo = 'F';
+
+select * from aluno where valor_pago_curso < 600;
+select * from aluno where valor_pago_curso > 600;
+select * from aluno where valor_pago_curso <= 600.55;
+select * from aluno where valor_pago_curso >= 600.55;
+
+select * from aluno;
+
+select * from aluno where sexo = 'M' AND ativo_sn = 1 AND valor_pago_curso <= 625;
+
+select * from aluno where sexo = 'F' OR idade >= 40;
+
